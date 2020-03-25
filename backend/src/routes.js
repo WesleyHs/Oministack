1d748ae4ba419e781ef1285a1ad3b42d464db2b5
Wesley <wesley.henrique.pgm@gmail.com>
2020-03-25 00:33:38 -0300

    pages

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const OngController = require('./controllers/OngController')
 const IncidentController = require('./controllers/Incidentcontroller')
 const ProfileController = require('./controllers/ProfileController')
-
+const SessionController = require('./controllers/SessionController')
 
 
 const routes = express.Router();
 
+routes.post('/sessions', SessionController.create)
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
