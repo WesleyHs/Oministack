1413e66c2585a10697711142a3a0945d40674686
Wesley <wesley.henrique.pgm@gmail.com>
2020-03-27 00:42:21 -0300

    incidents

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,11 +1,21 @@
 import React from 'react'
-import { View, Image, Text } from 'react-native'
+import { View, FlatList, Image, Text } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import { Feather } from '@expo/vector-icons';
 
 import logoImg from '../../assets/logo.png'
 
 import styles from './styles';
+import { setLightEstimationEnabled } from 'expo/build/AR';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default function Incidents() {
+    const navigation = useNavigation();
+
+    function navigateToDetail(){
+        navigation.navigate('Detail')
+    }
+
     return (
         <View style={styles.container}>
             <View style={styles.header}>
@@ -16,6 +26,31 @@ export default function Incidents() {
             </View>
             <Text style={styles.title}>Bem vindo</Text>
             <Text style={styles.description}> Escolha um dos casos abaixo e salve o dia. </Text>
+
+            <FlatList
+                data={[1, 2, 3, 4, 5, 6]}
+                style={styles.incidentList}
+                keyExtractor={incident => String(incident)}
+                showsVerticalScrollIndicator={false}
+                renderItem={() => (
+                    <View  style={styles.incident}>
+                        <Text style={styles.incidentProperty}> ONG:</Text>
+                        <Text style={styles.incidentValue}> APAD</Text>
+
+                        <Text style={styles.incidentProperty}>Valor:</Text>
+                        <Text style={styles.incidentValue}>R$ 120,00</Text>
+
+                        <TouchableOpacity
+                            style={styles.detailsButton}
+                            onPress={navigateToDetail}
+                        >
+                            <Text style={styles.detailsButtonText}> Ver mais detalhes</Text>
+                            <Feather name="arrow-right" size={16} color="E02041" />
+                        </TouchableOpacity>
+
+                    </View>
+                )}
+            />
 
         </View>
     )
